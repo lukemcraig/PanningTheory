d342ccdf22fbed0248bbea5d17926b05171b21b0
Luke M Craig <lukemcduffiecraig@gmail.com>
2018-04-21 02:25:40 -0400

    drawing a grid and a vector

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -32,9 +32,32 @@ void PanningTheoryAudioProcessorEditor::paint (Graphics& g)
 
     g.setColour (Colours::white);
 
-	auto transform = AffineTransform().translated(200,150);
+	auto transform = AffineTransform().scaled(150, -150).translated(200,150);
 	g.addTransform(transform);
-	g.drawArrow(Line<float>(0, 0, 1, 1), 2,1,1);
+
+	float majorGridLineThickness = .005f;
+	float minorGridLineThickness = .001f;
+	auto horizontalLine = Line<float>(-1,-1,1,-1);
+	auto verticalLine = Line<float>(-1, -1, -1, 1);
+	int numberOfMajorLines = 4;
+	int numberOfMinorLines = 4;
+	for (int i = 0; i < numberOfMajorLines; i++) {
+		g.drawLine(horizontalLine, majorGridLineThickness);
+		for (int i = 0; i < numberOfMinorLines; i++) {
+			horizontalLine.applyTransform(AffineTransform::translation(0, 0.5f / numberOfMinorLines));
+			g.drawLine(horizontalLine, minorGridLineThickness);
+		}		
+
+		g.drawLine(verticalLine, majorGridLineThickness);
+		for (int i = 0; i < numberOfMinorLines; i++) {
+			verticalLine.applyTransform(AffineTransform::translation(0.5f / numberOfMinorLines, 0));			
+			g.drawLine(verticalLine, minorGridLineThickness);
+		}
+	}	
+	g.drawLine(horizontalLine, majorGridLineThickness);
+	g.drawLine(verticalLine, majorGridLineThickness);
+
+	g.drawArrow(Line<float>(0, 0, 1, 1), .05,.2f,.2f);
 }
 
 void PanningTheoryAudioProcessorEditor::resized()
